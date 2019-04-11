f16e6c8edcafc91a1879364f4b2fc4566ef6ed86
David Barsky <dbarsky@amazon.com>
2019-04-11 13:42:39 -0400

    integ with iterm2

--- functions/theme-pure/fish_prompt.fish
+++ functions/theme-pure/fish_prompt.fish
@@ -3,7 +3,7 @@ function fish_prompt
 
     echo -e -n (_pure_prompt_beginning)  # init prompt context (clear current line, etc.)
     echo -e (_pure_prompt_first_line)  # print current path, git branch/status, command duration
-    echo -e -n (_pure_prompt $exit_code)  # print prompt
+    echo -e -n (iterm2_prompt_mark _pure_prompt $exit_code)  # print prompt
     echo -e (_pure_prompt_ending)  # reset colors and end prompt
 
     set _pure_fresh_session false
